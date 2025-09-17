e26e66bb174be92909360da94ce1825aafd336a2
MatheusT <93778832+MatheusTeles001@users.noreply.github.com>
2025-09-17 14:15:20 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./context/MovieContext";
+import NavBar from "./components/Navbar";
+
+function App() {
+  const movieNumber = 1;
+
+  return (
+    <div>
+      <MovieProvider>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </MovieProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,28 @@
+import "../css/favorites.css";
+import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites-page">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorie-empty">
+      <h2>Your favorite list is empty</h2>
+      <p>Add some movies to your favorite list.</p>
+    </div>
+  );
+}
+export default Favorites;
